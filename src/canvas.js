c4d86620b2df8c2c48fed0d499477f8ef8cadef2
Robert Bruce Gifford <timeonator@gmail.com>
2021-02-21 09:59:48 -0800

    doodling with arcs, paths and vectors

--- src/canvas.js
+++ src/canvas.js
@@ -7,8 +7,30 @@ const Canvas = (props) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.beginPath();
-    ctx.arc(150, 90, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.arc(120, 80, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
     ctx.fill();
+
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 3; j++) {
+        ctx.beginPath();
+        var x = 75 + j * 50; // x coordinate
+        var y = 25 + i * 50; // y coordinate
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+        var anticlockwise = i % 2 !== 0; // clockwise or anticlockwise
+
+        ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+        if (i > 1) {
+          ctx.fill();
+        } else {
+          ctx.stroke();
+        }
+      }
+    }
+    var p = new Path2D("M10 10 h 80 v 80 h -80 Z");
+    ctx.stroke(p);
   };
 
   useEffect(() => {
