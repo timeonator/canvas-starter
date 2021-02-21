0b3bb4fd02bda506473dae1c25f828602d8c3fa0
Robert Bruce Gifford <timeonator@gmail.com>
2021-02-20 19:23:12 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Canvas from "./canvas.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Canvas</h1>
+      <Canvas />
+    </div>
+  );
+}
